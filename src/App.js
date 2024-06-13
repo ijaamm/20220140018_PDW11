9840c0b156b416889bae9bde0ef6ac619d37b18c
ijaamm <ijammusyaf@gmail.com>
2024-06-13 18:03:18 +0700

    done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://logos-world.net/wp-content/uploads/2023/03/Straw-Hat-Logo.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          one piece
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+  href="https://www.google.com/search?q=one+piece&sca_esv=e912ca26f44d8cea&sxsrf=ADLYWIJHUkoGN2Grm2ljZLc2OZxQMU3nfA%3A1718275346981&ei=Es1qZrHIO5GVseMPmuun0Aw&ved=0ahUKEwixmbu9stiGAxWRSmwGHZr1CcoQ4dUDCBA&uact=5&oq=one+piece&gs_lp=Egxnd3Mtd2l6LXNlcnAiCW9uZSBwaWVjZTIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzIKEAAYsAMY1gQYRzINEAAYgAQYsAMYQxiKBTINEAAYgAQYsAMYQxiKBTINEAAYgAQYsAMYQxiKBTIOEAAYsAMY5AIY1gTYAQEyDhAAGLADGOQCGNYE2AEBMg4QABiwAxjkAhjWBNgBATITEC4YgAQYsAMYQxjIAxiKBdgBAjITEC4YgAQYsAMYQxjIAxiKBdgBAjITEC4YgAQYsAMYQxjIAxiKBdgBAjITEC4YgAQYsAMYQxjIAxiKBdgBAkj3DFAAWABwAXgBkAEAmAEAoAEAqgEAuAEDyAEAmAIBoAIJmAMAiAYBkAYSugYGCAEQARgJugYGCAIQARgIkgcBMaAHAA&sclient=gws-wiz-serp"
           target="_blank"
           rel="noopener noreferrer"
         >
